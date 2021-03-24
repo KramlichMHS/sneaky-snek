ebd43d0132c49e72a07e9472d83ba62e2a5c69e6
Rob Kramlich <67026814+KramlichMHS@users.noreply.github.com>
2021-03-24 10:57:25 -0400

    body parts drop

--- main.ts
+++ main.ts
@@ -1,10 +1,32 @@
 tiles.setTilemap(tilemap`backgroundMap`)
-let snek = sprites.create(assets.image`headRight`, 0)
+
+let snek = sprites.create(assets.image`headRight`, SpriteKind.Player)
 let x = 80
 let y = 110
 snek.setPosition(x, y)
-let foodOut = true
 let direction = "R"
+let bodyParts = [snek]
+
+let body = [[x,y]]
+
+info.player1.setScore(0)
+let food = sprites.create(img`
+            . . . . 2 2 . . . .
+            . . . 2 2 2 2 . . .
+            . . . 2 2 2 2 . . .
+            . . 2 2 2 2 2 2 . .
+            . . 2 2 2 2 2 2 . .
+            . 2 2 2 2 2 2 2 2 .
+            . 2 2 2 2 2 2 2 2 .
+            . 2 2 2 2 2 2 2 2 .
+            2 2 2 2 2 2 2 2 2 2
+            2 2 2 2 2 2 2 2 2 2
+        `, SpriteKind.Food)
+food.setPosition(randint(10, 150), randint(10,110))
+
+
+
+
 // set directions
 controller.right.onEvent(ControllerButtonEvent.Pressed, function rightPressed() {
     
@@ -38,6 +60,8 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function downPressed() {
     }
     
 })
+
+//movement
 game.onUpdateInterval(500, function movement() {
     
     if (direction == "R") {
@@ -49,6 +73,32 @@ game.onUpdateInterval(500, function movement() {
     } else {
         y += 10
     }
-    
+    /*
+    let temp = body[0]
+    for(let i = 0; i < body.length; i++){
+        temp = body[i]
+
+    }
+    */
     snek.setPosition(x, y)
 })
+
+//food overlap
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function(sprite: Sprite, otherSprite: Sprite) {
+    info.player1.changeScoreBy(1)
+    otherSprite.setPosition(randint(10,150), randint(10,110))
+    let bodyPart = sprites.create(img`
+        . 5 5 5 5 5 5 5 .
+        5 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5
+        5 5 5 5 5 5 5 5 5
+        . 5 5 5 5 5 5 5 .
+    `)
+    bodyPart.setPosition(x, y)
+})
+
+
